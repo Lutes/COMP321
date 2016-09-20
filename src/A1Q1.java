802d6de4b46c432275c502c43132c450a52f5a9f
Lutes <Daniel.Lutes@mail.mcgill.ca>
2016-09-19 21:29:37 -0400

    Solution set up in Kattis Format.

--- src/A1Q1.java
+++ src/A1Q1.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 public class A1Q1 {
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
+		while (sc.hasNextInt()) {
+		   int number = sc.nextInt();
+		   solve(number);
+		}
+	}	
+	
+	public static void solve(int number){		
 		int sum = sumInt(number);
 		int solution = 0;
+		
 		if((0 != sum)){
 			for (int i = 11; i < 100000; i++){
 				if (sumInt(i * number) == sum){
@@ -17,6 +24,7 @@ public class A1Q1 {
 			System.out.println(solution);
 		}
 	}
+
 	
 	public static int[] intToArray(int number){
 		String temp = Integer.toString(number);
